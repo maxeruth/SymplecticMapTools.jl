d51520c871e09aecbd50c3ab641f755bd71ada7d
Maximilian Ruth <max@wireless-10-150-167-168.public.utexas.edu>
2024-08-29 12:12:40 -0500

    Updated Documentation

--- docs/literate_examples.jl
+++ docs/literate_examples.jl
@@ -2,9 +2,9 @@ using Literate
 using Base
 execute = true 
 # ENV["JULIA_DEBUG"]="Literate"
-# Literate.notebook("./extrapolation.jl", pwd()*"/../examples/"; execute)
-# Literate.markdown("./extrapolation.jl", pwd()*"/../docs/src/examples/extrapolation/"; execute)
-# Literate.notebook("./birkhoff_averaging.jl", pwd()*"/../examples/"; execute)
-# Literate.markdown("./birkhoff_averaging.jl", pwd()*"/../docs/src/examples/birkhoff_averaging/"; execute)
+Literate.notebook("./extrapolation.jl", pwd()*"/../examples/"; execute)
+Literate.markdown("./extrapolation.jl", pwd()*"/../docs/src/examples/extrapolation/"; execute)
+Literate.notebook("./birkhoff_averaging.jl", pwd()*"/../examples/"; execute)
+Literate.markdown("./birkhoff_averaging.jl", pwd()*"/../docs/src/examples/birkhoff_averaging/"; execute)
 Literate.notebook("./kernel.jl", pwd()*"/../examples/"; execute)
 Literate.markdown("./kernel.jl", pwd()*"/../docs/src/examples/kernel/"; execute)
